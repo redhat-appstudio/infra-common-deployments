75f5365ecda4f5db6d6261170091ecc581be043e
Francesco Ilario <filario@redhat.com>
2026-01-16 15:31:51 +0100

    add chainsaw tests for kyverno policies (#167)
    * add chainsaw tests for kyverno policies
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    * fix kube-linter complaints
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    ---------
    
    Signed-off-by: Francesco Ilario <filario@redhat.com>

---
+++ hack/chainsaw/chainsaw-prepare.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+## Create the kind cluster
+kind create cluster --name infra-common-deployments-chainsaw
+
+## Install kyverno
+kustomize build --enable-helm components/kyverno/chainsaw | \
+  kubectl apply -f - --server-side
+
+## wait for kyverno to rollout
+kubectl rollout status deployment \
+  --namespace konflux-kyverno \
+  --selector '!job-name' \
+  --timeout=300s
